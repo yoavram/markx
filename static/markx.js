c28023901290d108242dac525916c6d66f9037b2
Yoav Ram <yoavram+github@gmail.com>
2012-12-24 20:53:11 +0200

    added word count

--- static/markx.js
+++ static/markx.js
@@ -191,3 +191,34 @@ function save_output(extension, callack) {
 	});
 }
 
+/*
+ * http://roshanbh.com.np/2008/10/jquery-plugin-word-counter-textarea.html
+ * Textarea Word Count Jquery Plugin 
+ * Version 1.0
+ * 
+ * Copyright (c) 2008 Roshan Bhattarai
+ * website : http://roshanbh.com.np
+ * 
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+*/
+
+jQuery.fn.wordCount = function(params){
+	var p = {
+		counterElement:"display_count"
+	};
+	var total_words;
+	
+	if(params) {
+		jQuery.extend(p, params);
+	}
+	
+	//for each keypress function on text areas
+	this.keypress(function()
+	{ 
+		total_words=this.value.split(/[\s\.\?]+/).length;
+		jQuery('#'+p.counterElement).html(total_words);
+	});	
+};
