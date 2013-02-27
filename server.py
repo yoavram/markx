32e7427e45c4b281a0a4a393f07fa98e2701d086
Walter, Mathias <mathias.walter@helmholtz-muenchen.de>
2013-02-27 17:09:38 +0100

    fixing SCRIPT_NAME/url_scheme when behind reverse proxy

--- server.py
+++ server.py
@@ -27,9 +27,49 @@ with open(DEFAULT_TEXT_FILE,'r') as f:
 PANDOC_EXTENSIONS = ['pdf', 'docx', 'epub', 'html']    
 DOCVERTER_URL =   'http://c.docverter.com/convert'
 PDFLATEX_EXISTS = distutils.spawn.find_executable("pdflatex") != None
-STATIC_URL_PATH = '/markx/static' # flask default
 
-app = Flask(__name__, static_url_path=STATIC_URL_PATH)
+class ReverseProxied(object):
+    '''Wrap the application in this middleware and configure the 
+    front-end server to add these headers, to let you quietly bind 
+    this to a URL other than / and to an HTTP scheme that is 
+    different than what is used locally.
+
+    In nginx:
+    location /myprefix {
+        proxy_pass http://192.168.0.1:5000;
+        proxy_set_header Host $host;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Scheme $scheme;
+        proxy_set_header X-Script-Name /myprefix;
+        }
+
+    In Apache2:
+    <Location /markx> 
+        ProxyPass http://192.168.0.1:5000
+        ProxyPassReverse http://192.168.0.1:5000
+        RequestHeader set X-Script-Name /markx
+    </Location>
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.app(environ, start_response)
+
+app = Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
 app.config.from_object(__name__) 
 print " * Overriding deafult configuration with config.py file"
 app.config.from_pyfile('config.py', silent=True)
